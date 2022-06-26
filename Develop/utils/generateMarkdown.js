3eeab6b10d94d8f3fd43e7e807f6d0455a02f90f
SMazariego <102334091+SMazariego@users.noreply.github.com>
2022-06-26 15:09:05 -0400

    corrections

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,47 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
+function renderLicenseBadge(license) {
+if (license === "None") {
+  return "";
+} else {
+  return `![badge-image](https://img.shields.io/static/v1?label=license&message=${license.replace(
+    " ",
+    "-"
+  )}&color=green)`;
+}
+}
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `https://choosealicense.com/licenses/${license
+      .toLowerCase()
+      .replace(" ", "-")}/`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
+function renderLicenseSection(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    licenseLink = renderLicenseLink(license);
+    return `## License
+    
+    This project is licensed under the ${license} license.
+    
+    The link to the license is at ${licenseLink}.
+    `;
+  }
+}
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
 
-`;
-}function generateMarkdown(data) {
-  let licenseBadge = renderLicenseBadge(data.projectLicense);
-  let licenseSection = renderLicenseSection(data.projectLicense);
+function generateMarkdown(data) {
+  let licenseBadge = renderLicenseBadge(data.license);
+  let licenseSection = renderLicenseSection(data.license);
   return `# ${data.Title}
   ${licenseBadge}
 ## Description
