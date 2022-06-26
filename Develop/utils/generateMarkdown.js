f94bf4e80d0057091b30db2afb6875c1dc70f7af
SMazariego <102334091+SMazariego@users.noreply.github.com>
2022-06-26 15:05:40 -0400

    error corrections

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,14 @@
-function renderLicenseBadge(license) {
-  if (license === "None") {
-    return "";
-  } else {
-    return `![badge-image](https://img.shields.io/static/v1?label=license&message=${license.replace(
-      " ",
-      "-"
-    )}&color=green)`;
-  }
-}
+// TODO: Create a function that returns a license badge based on which license is passed in
+// If there is no license, return an empty string
+function renderLicenseBadge(license) {}
 
-function renderLicenseLink(license) {
-  if (license === "None") {
-    return "";
-  } else {
-    return `https://choosealicense.com/licenses/${license
-      .toLowerCase()
-      .replace(" ", "-")}/`;
-  }
-}
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
@@ -42,8 +33,8 @@ ${data.contribution}
 ${licenseSection}
 ## Questions?
 Please contact me at the links below for any inquiries.
-Github: https://github.com/${data.githubUsername}
-Email: ${data.emailAddress}
+Github: https://github.com/${data.github}
+Email: ${data.email}
 `;
 }
 
