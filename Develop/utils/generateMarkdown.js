e56230114e9fe23313e3800e1b38b62cc5ba1228
SMazariego <102334091+SMazariego@users.noreply.github.com>
2022-06-26 14:39:16 -0400

    question generate

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -15,6 +15,27 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
 `;
+}function generateMarkdown(data) {
+  let licenseBadge = renderLicenseBadge(data.projectLicense);
+  let licenseSection = renderLicenseSection(data.projectLicense);
+  return `# ${data.Title}
+  ${licenseBadge}
+## Description
+${data.Description}
+## Installation
+${data.installation}
+## Usage
+${data.Instructions}
+## Testing
+${data.test}
+## Contribute
+${data.contribution}
+${licenseSection}
+## Questions?
+Please contact me at the links below for any inquiries.
+Github: https://github.com/${data.githubUsername}
+Email: ${data.emailAddress}
+`;
 }
 
 module.exports = generateMarkdown;
