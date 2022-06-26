c2a74355286969f0a2c918e69046d7414285efd2
SMazariego <102334091+SMazariego@users.noreply.github.com>
2022-06-26 14:52:58 -0400

    license info

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,14 +1,23 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `![badge-image](https://img.shields.io/static/v1?label=license&message=${license.replace(
+      " ",
+      "-"
+    )}&color=green)`;
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseLink(license) {
+  if (license === "None") {
+    return "";
+  } else {
+    return `https://choosealicense.com/licenses/${license
+      .toLowerCase()
+      .replace(" ", "-")}/`;
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
